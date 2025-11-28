83bb663705cad2476526b4928088681197c16b5f
Bruno Lima da Silva <ozamodas123@gmail.com>
2025-11-28 11:57:49 -0300

    primeiro commit

---
+++ main.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <assert.h>
+
+
+typedef struct{
+    unsigned char* buffer;
+    size_t length;
+    size_t offset;
+} Arena;
+
+void arena_init(Arena* a, size_t size){
+    a->buffer = malloc(size);
+    if(!a->buffer) exit(1);
+
+    a->offset = 0;
+    a->length = size;
+}
+
+void arena_reset(Arena* a){
+    a->offset = 0;
+}
+
+void* arena_malloc(Arena* a,size_t size){
+    //
+    assert(size <= a->length - a->offset); //verifica a condicao
+    void* ptr = &a->buffer[a->offset]; //ponteiro que aponta para o topo
+    a->offset += size; //deslocando
+    return ptr;
+}
+
+int main(){
+    Arena* arena;
+    arena_init(arena,(1024*1024*10)); //iniciando a arena
+    
+    printf("%ld\n", arena->offset); //verificando valor de offset
+    void* ponteiro = arena_malloc(arena,10*(sizeof(int))); //alocando memoria
+    printf("%ld\n", arena->offset); //verificamos o novo valor de offset
+    printf("%p\n", ponteiro); //vericamos se o ponteiro recebeu o endereço de memoria
+    arena_reset(arena); //resetando nossa arena
+    printf("%ld\n", arena->offset); //valor de offset aqui deve ser 0, pois foi resetado
+    return 0;
+}
